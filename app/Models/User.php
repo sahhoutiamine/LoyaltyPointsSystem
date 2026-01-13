3fad16d783e27c248d0a76ab667f45fac439ab6c
sahhoutiamine <midoriaduko@gmail.com>
2026-01-13 11:21:38 +0100

    add create method

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,7 +11,20 @@ class User
         $this->db = $db;
     }
     
- 
+    public function create($data)
+    {
+        $sql = "INSERT INTO {$this->table} (email, password_hash, name, created_at) 
+                VALUES (:email, :password_hash, :name, NOW())";
+        
+        $stmt = $this->db->prepare($sql);
+        $stmt->execute([
+            ':email' => $data['email'],
+            ':password_hash' => password_hash($data['password'], PASSWORD_BCRYPT),
+            ':name' => $data['name']
+        ]);
+        
+        return $this->db->lastInsertId();
+    }
     
 }
 ?>
